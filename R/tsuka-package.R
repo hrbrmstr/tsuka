53464cd15e6a73f017b6c026b06fec5643eef5a7
boB Rudis <bob@rud.is>
2019-08-09 16:46:32 -0400

    R package repo initialization complete

---
+++ R/tsuka-package.R
@@ -0,0 +1,12 @@
+#' ...
+#' 
+#' - URL: <https://gitlab.com/hrbrmstr/tsuka>
+#' - BugReports: <https://gitlab.com/hrbrmstr/tsuka/issues>
+#' 
+#' @md
+#' @name tsuka
+#' @keywords internal
+#' @author Bob Rudis (bob@@rud.is)
+#' @import httr
+#' @importFrom jsonlite fromJSON
+"_PACKAGE"
